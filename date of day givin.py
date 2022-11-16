79a2c22dbb95a38f8eab1eb78322b6fd7b08fd65
ahmedbarakat2007 <118398763+ahmedbarakat2007@users.noreply.github.com>
2022-11-16 23:47:25 +0200

    Add files via upload

--- date of day givin.py
+++ date of day givin.py
@@ -1,3 +1,4 @@
+#this is the import of time and calender to make the programe work
 import datetime
 import calendar
 
@@ -13,12 +14,18 @@ print ("____________________")
 print ("   Enter the year   ")
 print ("____________________")
 year = input ()
-
+#this just take the day from the calender DON'T TOUCH IT if you don't know what are you doing
+#copy it if you need this part only
 def findDay(date):
     born = datetime.datetime.strptime(date, '%d %m %Y').weekday()
     return (calendar.day_name[born])
+#that's the end of copying :)
+#Its basicly jusr a day/month/year that written like that for example 10 09 2013
+# the " " is just a space to separate between day, month and year
 weekday = (day + " " + month + " " + year)
 print("______________")
+#findDay is basicly just finds the day of your givin date
+#it's important too and in between () you type the variable
 print("   " + findDay(weekday) + "   ")
 print("______________")
 
